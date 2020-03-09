c3bbd1521aadfd31f5c282046aa7497ccb0a638f
rricoldi <renan.ricoldi@uel.br>
2020-03-09 09:17:55 -0300

    Comentário do código

--- chat_sockets_tcp/client.py
+++ chat_sockets_tcp/client.py
@@ -4,7 +4,7 @@ from threading import Thread
 HOST = '127.0.0.1'  # Endereço IP
 PORT = 8080           # Porta a ser escutada
 
-def receber():
+def receber():  # Função que recebe as mensagens do servidor e mostra ao cliente
     while True:
         try:
             mensagem = server.recv(1024).decode("utf8")
@@ -13,20 +13,20 @@ def receber():
             break
 
 
-def enviar():
+def enviar(): # Função que recebe mensagens do cliente e as envia ao servidor
   while True:
     mensagem = input()
 
     server.send(bytes(mensagem, "utf8"))
-    if mensagem == "sair()":
+    if mensagem == "sair()":  # Caso a mensagem seja "sair()" fecha a conexão
       server.close()
       break
       
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect((HOST, PORT))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Cria o socket
+server.connect((HOST, PORT))  # Conecta ao servidor
 
 print('Para sair use a função sair()\n')
 
-Thread(target=receber).start()
+Thread(target=receber).start()  # Threads para receber e enviar ao mesmo tempo
 Thread(target=enviar).start()
--- chat_sockets_tcp/server.py
+++ chat_sockets_tcp/server.py
@@ -4,27 +4,27 @@ from threading import Thread
 IP = '127.0.0.1'  # Endereço IP
 PORTA = 8080           # Porta a ser escutada
 
-def conexao():
+def conexao():  # Função para lidar com conexão de novos clientes
     while True:
-        connection, address = server.accept()
-        lista_de_clientes.append(connection)
+        connection, address = server.accept()   # Aceitando um novo cliente e recebendo as informações deste
+        lista_de_clientes.append(connection)    # Introdução do cliente à lista de clientes
         
         print('Conexão ativa com ', address)
 
-        Thread(target=chat, args = (connection, address)).start()
+        Thread(target=chat, args = (connection, address)).start()   # Nova thread para lidar com as mensagens do cliente conectado
 
 def chat(connection, address):
     while True:
-        mensagem = connection.recv(1024)
+        mensagem = connection.recv(1024)    # Recebimento de uma mensagem
         
-        if mensagem == bytes("sair()", "utf8"):            
+        if mensagem == bytes("sair()", "utf8"): # Casi receba a mensagem sair fechará a conexão e removerá o cliente da lista de clientes            
             lista_de_clientes.remove(connection)
             connection.close()
             break
         
         mensagem = bytes('<' + address[0] + '> ', 'utf8') + mensagem
 
-        for cliente in lista_de_clientes:
+        for cliente in lista_de_clientes:   # Envia a mensagem a todos os clientes exceto quem enviou
             if cliente != connection: 
                 try: 
                     cliente.send(mensagem)
@@ -34,14 +34,14 @@ def chat(connection, address):
     if lista_de_clientes.__len__() == 0:
         print('Não há clientes ativos!')
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Cria o socket
 
-server.bind((IP, PORTA))
-server.listen(PORTA)
+server.bind((IP, PORTA))    # Atribui o ip e porta definidos ao socket
+server.listen(PORTA)    # Socket começa a ouvir o endereço
 
 lista_de_clientes = []
 
-t = Thread(target=conexao())
+t = Thread(target=conexao())    # Thread principal
 t.start()
 
-server.close()
+server.close()  # Finaliza o socket
